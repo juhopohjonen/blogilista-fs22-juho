d8ecd5461612237fa71aa4138115526466432ae4
Juho Pohjonen <juhopohjonen237@gmail.com>
2022-07-17 12:23:29 +0300

    tehtävät valmistuu

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 
 const app = express()
@@ -18,5 +19,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
